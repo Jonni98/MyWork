fb7f74aa097add66899edfaabbba39ae86bef151
Joni <jonni_98@icloud.com>
2020-12-24 00:40:09 +0300

    init

---
+++ js-code.js
@@ -0,0 +1,44 @@
+// const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies:{},
+//     actors:{},
+//     genres:[],
+//     privat: false
+// };
+
+// const a = prompt('один из последних просмотренных фильмов?', ''),
+//         b = prompt('насколько оцените его?', ''),
+//         c = prompt('один из последних просмотренных фильмов?', ''),
+//         d = prompt('насколько оцените его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[b] = d;
+
+// console.log(personalMovieDB);
+
+// let num = 50;
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(num);
+//     num++;
+// }
+let num = 50;
+for (let i = 1; i < 500; i++) {
+   if (i === 11) {
+       continue;
+   }
+    console.log(i);
+}
